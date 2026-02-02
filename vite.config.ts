a1eb3c2f4355040f69d7a7bc356dee3f1774a862
abmoizs <amsdigitalservices2k13@gmail.com>
2026-02-02 14:34:22 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/drjavariafitness/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
